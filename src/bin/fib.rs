bc3d65bae2b2dd8ba8721588c514a4e7755ced5c
Michal Katanski <mike.katanski@gmail.com>
2019-03-09 22:29:03 +0100

    Some refactor

--- src/bin/fib.rs
+++ src/bin/fib.rs
@@ -2,9 +2,9 @@ use std::io;
 
 fn fib(n: u32) -> u32 {
   if n <= 1 {
-    return n;
+    n
   } else {
-    return fib(n-1) + fib(n-2);
+    fib(n-1) + fib(n-2)
   }
 }
 
@@ -14,9 +14,7 @@ fn main() {
   let mut input = String::new();
 
   io::stdin().read_line(&mut input).expect("Enter fibonacci position!");
-
   let position: u32 = input.trim().parse().expect("Enter number");
-
   let res = fib(position);
 
   println!("Fibonacci: {}", res);
