7c5ce73b2e596f2696e1a29ddbd5bfe4e0b19c90
Michal Katanski <mike.katanski@gmail.com>
2019-03-09 21:39:39 +0100

    fahrenheit to celsius

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter Fahrenheit temperature");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Please enter Fahrenheit temperature");
+    let fahrenheit: f32 = input.trim().parse().expect("Provided temperature must be a number");
+
+    let celsius: f32 = (fahrenheit - 32.0) * 5.0/9.0;
+
+    println!("Celsius: {:.1}", celsius);
 }
